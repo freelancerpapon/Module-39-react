b2142329ff3ce536e8a41a89fb94c9c6ec7ad070
Papon Kumar Das <138362995+freelancerpapon@users.noreply.github.com>
2023-09-09 18:46:49 +0600

    Delete explore-react-core-concepts-part2-main directory

--- explore-react-core-concepts-part2-main/src/Friend.jsx
+++
@@ -1,9 +0,0 @@
-export default function Friend({friend}){
-    const {name, email} = friend;
-    return (
-        <div className="box">
-            <h4>Name: {name} </h4>
-            <p>Email: {email}</p>
-        </div>
-    )
-}
